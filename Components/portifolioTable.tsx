6ec57d11fd1b59efdb29ba04a1b64b7c4f9ef6c2
jayavishvaa <me18b110@smail.iitm.ac.in>
2025-12-22 23:52:51 +0530

    complete portfolio dashboard with live CMP and fundamentals

---
+++ Components/portifolioTable.tsx
@@ -0,0 +1,410 @@
+'use client'
+
+import React, {useMemo, useState, useEffect} from 'react';
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable
+} from '@tanstack/react-table';
+import { table_row } from '../Data/portifolio';
+import axios from 'axios'
+import { BeatLoader } from 'react-spinners'
+
+type Props = {
+    data: table_row[]
+}
+
+
+export default function PortfolioTable({data}: Props) {
+    
+    const [cmpData, setCmpData] = useState<Record<string, number>>({})
+    const [fundamentalData, setFundamentalData] = 
+        useState<Record<string, {peRatio: number | null; lastestEarning: number | null}>>({})
+    const [cmpLoading, setCmpLoading] = useState(false)
+    const [fundamentalLoading, setFundamentalLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+
+    // Fetch CMP Data
+
+    useEffect(() => {
+        if (!data || data.length === 0) return
+
+        let intervalId: NodeJS.Timeout
+        const fetchCmpData = async () => {
+            setCmpLoading(true)
+            setError(null)
+
+            try {
+                const symbols = data.map(d => d.exchange_code).join(',')
+
+                const response = await axios.get(
+                    'http://localhost:5001/api/cmp',
+                    {
+                        params: { symbols }
+                    }
+                )
+
+                const result = response?.data
+                setCmpData(result)
+
+            }
+            catch (err) {
+                console.error('CMP fetch failed',err)
+                setError('Failed to fetch CMP data')
+                setCmpData({})
+            }
+            finally {
+                setCmpLoading(false)
+            }
+        }
+
+        // Periodic API call
+        if (data.length > 0) {
+            fetchCmpData()
+        
+            intervalId = setInterval(() => {
+              fetchCmpData()
+            }, 60_000) // every 60 seconds
+        }
+        
+        return () => {
+            if (intervalId) clearInterval(intervalId)
+          }
+    },[data])
+
+    // Fetch P/e ratio and LES data
+
+    useEffect(() => {
+
+        if (!data || data.length === 0) return
+        const fetchFundamentals = async () => {
+            setFundamentalLoading(true)
+
+            try {
+                const uniqueStocks = Array.from(
+                    new Set(data.map(d =>`${d.exchange_code}-${d.exchange}`))
+                )
+
+                const newEntries: Record<string, any> = {}
+
+                await Promise.all(
+                    uniqueStocks.map(async key => {
+                        const [code, exchange] = key.split('-')
+
+                        if (fundamentalData[code]) return
+
+                        try {
+                            const res = await axios.get(
+                                'http://localhost:5001/api/fundamentals',
+                                {
+                                    params: {
+                                        symbol: code,
+                                        exchange: exchange === 'NSE' ? 'NSE' : 'BOM'
+                                    }
+                                }
+                            )
+                            newEntries[code] = res.data
+                        } 
+                        catch (err) {
+                            console.error(`Error fetching ${code}:`, err)
+                        }
+                    })
+                )
+                // Update state once with all new items
+                if (Object.keys(newEntries).length > 0) {
+                    setFundamentalData(prev => ({ ...prev, ...newEntries }))
+                }
+            }
+            finally {
+                setFundamentalLoading(false)
+            }
+        }
+        fetchFundamentals()
+    },[data])
+
+    // Calculate Total Investment
+
+    const total_investment = useMemo(() => {
+        return data.reduce((sum, stock) => {
+            return sum + (stock.purchase_price * stock.quantity)
+        }, 0)
+    },[data])
+
+    // Calculate Total Present value
+
+    const total_pv = useMemo(() => {
+        return data.reduce((sum, stock) => {
+            const cmp = cmpData[stock.exchange_code]
+            if (!cmp) return sum
+            return sum + cmp * stock.quantity
+        }, 0)
+    },[data, cmpData])
+
+    const total_gl = total_pv - total_investment
+
+    // Sector Grouping
+    
+    const sectorGroups = useMemo(() => {
+        const groups: Record<string, table_row[]> = {}
+
+        data.forEach(stock => {
+            if (!groups[stock.sector]) {
+                groups[stock.sector] = []
+            }
+            groups[stock.sector].push(stock)
+        })
+        return groups
+    },[data])
+
+    // Total Investment sector-wise
+
+    const sectorInvestment = (stocks: table_row[]) => (
+        stocks.reduce((sum, s) => sum + (s.purchase_price * s.quantity),
+        0
+        )
+    )
+
+    // Total Present value sector-wise
+
+    const sectorPresentValue = (stocks: table_row[]) => (
+        stocks.reduce((sum, s) => {
+            const cmp = cmpData[s.exchange_code]
+            if (!cmp) return sum
+            const total = sum + (cmp * s.quantity)
+            return total
+        },
+        0
+        )
+    )
+
+    const columns = useMemo<ColumnDef<table_row>[]>(
+        () => [
+            {
+                header: 'Particulars',
+                accessorKey: 'name'
+            },
+            {
+                header: 'Purchase Price',
+                accessorKey: 'purchase_price'
+            },
+            {
+                header: 'Qty',
+                accessorKey: 'quantity'
+            },
+            {
+                header: 'CMP',
+                cell: ({row}) => {
+                    const code = row.original.exchange_code
+                    console.log(code, cmpData[code], 'code')
+                    const cmp = cmpData[code]
+                    return cmp ? cmp.toLocaleString() : 'N/A'
+                }
+            },
+            {
+                header: 'Present Value',
+                cell: ({row}) => {
+                    const {quantity, exchange_code} = row.original
+                    const cmp = cmpData[exchange_code]
+                    
+                    if (!cmp) return 'N/A'
+
+                    const pv = cmp * quantity
+                    return pv.toFixed(2)
+                }
+            },
+            {
+                header: 'Gain / Loss',
+                cell: ({row}) => {
+                    const {exchange_code, purchase_price, quantity} = row.original
+                    const cmp = cmpData[exchange_code]
+                    const investment = purchase_price * quantity
+
+                    if (!cmp) return 'N/A'
+
+                    const pv = cmp * quantity
+                    const gl = pv - investment
+                    const isGain = gl >= 0
+
+                    return (
+                        <span className={isGain ? 'text-green-600' : 'text-red-600'}>
+                            {gl.toFixed(2).toLocaleString()}
+                        </span>
+                    )
+                }
+            },
+            {
+                header: 'Investment',
+                cell: ({row}) => {
+                    const {purchase_price,quantity} = row.original
+                    const investment = purchase_price * quantity
+                    return investment.toLocaleString()
+                }
+            },
+            {
+                header: 'Portfolio %',
+                cell: ({row}) => {
+                    const { purchase_price, quantity} = row.original
+                    const investment = purchase_price * quantity 
+                    const percentage = (investment / total_investment) * 100
+                    return percentage.toFixed(2) + '%'
+                }
+            },
+            {
+                header: 'P/E',
+                cell: ({ row }) => {
+                  const code = row.original.exchange_code
+                  const pe = fundamentalData[code]?.peRatio
+                  return pe !== null && pe !== undefined ? pe.toFixed(2) : 'N/A'
+                }
+            },
+            {
+                header: 'Latest Earnings',
+                cell: ({ row }) => {
+                  const code = row.original.exchange_code
+                  const earning = fundamentalData[code]?.lastestEarnings
+                  return earning !== null && earning !== undefined
+                    ? earning.toFixed(2)
+                    : 'N/A'
+                }
+            },
+            {
+                header: 'Exchange',
+                accessorKey: 'exchange'
+            },
+            {
+                header: 'Code',
+                accessorKey: 'exchange_code'
+            },
+        ],
+        [cmpData, total_investment, fundamentalData]
+    )
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel()
+    })
+
+    return (
+        <div className='overflow-x-auto'>
+
+            {/*  CMP Loading Indicator */}
+            {cmpLoading && (
+                <div className="flex items-center gap-2 text-sm text-gray-600 mb-2">
+                    <BeatLoader size={8} color="#4B5563" />
+                    <span>Updating live prices…</span>
+                </div>
+            )}
+
+             {/*  Fundament Data Loading Indicator */}
+            {fundamentalLoading && (
+                <div className="flex items-center gap-2 text-sm text-blue-600 mb-2">
+                    <BeatLoader size={8} color="#2563EB" />
+                    <span>Loading fundamentals…</span>
+                </div>
+            )}
+
+            {/*  Portfolio Table */}
+            <table className='w-full border text-sm'>
+                <thead className='bg-gray-400'>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id} className='border p-2 text-left'>
+                                    {flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext()
+                                    )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+
+                <tbody>
+                    {Object.entries(sectorGroups).map(([sector, stocks]) => {
+                        const investment = sectorInvestment(stocks)
+                        const presentValue = sectorPresentValue(stocks)
+                        const gainLoss = presentValue - investment
+
+                        return (
+                            <React.Fragment key={sector}>
+                                {/* Sector Calculation */}
+                                <tr className='bg-gray-200 font-semibold border p-1'>
+                                    <td colSpan={3} className='border p-1'>{sector}</td>
+                                    <td className='border p-1 text-center'>-</td>
+                                    <td className='border p-1 text-center'>{presentValue.toFixed(2)}</td>
+                                    <td className={gainLoss >= 0 ? 'text-green-600 text-center' : 'text-red-600 text-center'}>
+                                        {gainLoss.toFixed(2)}
+                                    </td>
+                                    <td className='border p-1 text-center'>{investment.toLocaleString()}</td>
+                                    <td className='border p-1 text-center'>-</td>
+                                    <td className='border p-1 text-center'>-</td>
+                                    <td className='border p-1 text-center'>-</td>
+                                    <td className='border p-1 text-center'>-</td>
+                                    <td className='border p-1 text-center'>-</td>
+                                </tr>
+
+                                {/* Stock rows */}
+                                {stocks.map(stock => {
+                                    const cmp = cmpData[stock.exchange_code]
+                                    const inv = stock.purchase_price * stock.quantity
+                                    const pv = cmp ? cmp * stock.quantity : null
+                                    const gl = pv !== null ? pv - inv : null
+                                    const PortfolioPercentage = ((inv / total_investment) * 100).toFixed(2)
+
+                                    return (
+                                        <tr key={stock.exchange_code} className='border text-center'>
+                                            <td className='border p-1 text-left'>{stock.name}</td>
+                                            <td className='border p-1 '>{stock.purchase_price}</td>
+                                            <td className='border p-1 '>{stock.quantity}</td>
+                                            <td className='border p-1 '>{cmp ? cmp.toFixed(2) : "N/A"}</td>
+                                            <td className='border p-1 '>{pv ? pv.toFixed(2) : 'N/A'}</td>
+                                            <td className={
+                                                `border border-black p-1 ${
+                                                    gl === null
+                                                      ? ''
+                                                      : gl >= 0
+                                                        ? 'text-green-600'
+                                                        : 'text-red-600'
+                                                  }`}>
+                                                {gl ? gl.toFixed(2) : 'N/A'}</td>
+                                            <td className='border p-1 '>{inv}</td>
+                                            <td className='border p-1 '>{PortfolioPercentage + '%'}</td>
+                                            <td className="border p-1">
+                                                {fundamentalData[stock.exchange_code]?.peRatio ?? 'N/A'}
+                                            </td>
+                                            <td className="border p-1">
+                                                {fundamentalData[stock.exchange_code]?.lastestEarnings ?? 'N/A'}
+                                            </td>
+                                            <td className='border p-1 '>{stock.exchange}</td>
+                                            <td className='border p-1 '>{stock.exchange_code}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </React.Fragment>
+                        )
+                    })}
+                
+                    <tr className='bg-black text-white font-bold p-1 text-center'>
+                        <td colSpan={3} className='p-1 border'>
+                            Total Portfolio
+                        </td>
+                        <td className='p-1 border'>-</td>
+                        <td className='p-1 border'>{total_pv.toFixed(2)}</td>
+                        <td className={`p-1 border border-white ${total_gl >= 0 ? 'text-green-400' : 'text-red-400'}`}>
+                            {total_gl.toFixed(2)}
+                        </td>
+                        <td className='p-1 border'>{total_investment.toLocaleString()}</td>
+                        <td className='p-1 border'>-</td>
+                        <td className='p-1 border'>-</td>
+                        <td className='p-1 border'>-</td>
+                        <td className='p-1 border'>-</td>
+                        <td className='p-1 border'>-</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
---
+++ Data/portifolio.ts
@@ -0,0 +1,254 @@
+export type table_row = {
+    name: string
+    purchase_price: number
+    quantity: number
+    exchange: 'NSE' | 'BSE'
+    exchange_code: string
+    sector: string
+}
+
+export const mock_cmp_date: mock_data<string, number> = {
+    'HDFCBANK': 600,
+    'BAJFINANCE': 7200,
+    '532174': 1120,
+    'AFFLE': 1380,
+    'LTIM': 5150,
+    'DMART': 4100
+}
+
+export const portfolio: table_row[] = [
+    {
+        name: 'HDFC Bank',
+        purchase_price: 1490,
+        quantity: 50,
+        exchange: 'NSE',
+        exchange_code: 'HDFCBANK',
+        sector: 'Financials'
+      },
+      {
+        name: 'Bajaj Finance',
+        purchase_price: 6466,
+        quantity: 15,
+        exchange: 'NSE',
+        exchange_code: 'BAJFINANCE',
+        sector: 'Financials'
+      },
+      {
+        name: 'ICICI Bank',
+        purchase_price: 780,
+        quantity: 84,
+        exchange: 'BSE',
+        exchange_code: '532174',
+        sector: 'Financials'
+      },
+      {
+        name: 'Bajaj Housing',
+        purchase_price: 130,
+        quantity: 504,
+        exchange: 'BSE',
+        exchange_code: '544252',
+        sector: 'Financials'
+      },
+      {
+        name: 'Savani Financials',
+        purchase_price: 24,
+        quantity: 1080,
+        exchange: 'BSE',
+        exchange_code: '511577',
+        sector: 'Financials'
+      },
+    
+      // Technology Sector
+      {
+        name: 'Affle India',
+        purchase_price: 1151,
+        quantity: 50,
+        exchange: 'NSE',
+        exchange_code: 'AFFLE',
+        sector: 'Technology'
+      },
+      {
+        name: 'LTI Mindtree',
+        purchase_price: 4775,
+        quantity: 16,
+        exchange: 'NSE',
+        exchange_code: 'LTIM',
+        sector: 'Technology'
+      },
+      {
+        name: 'KPIT Tech',
+        purchase_price: 672,
+        quantity: 61,
+        exchange: 'BSE',
+        exchange_code: '542651',
+        sector: 'Technology'
+      },
+      {
+        name: 'Tata Tech',
+        purchase_price: 1072,
+        quantity: 63,
+        exchange: 'BSE',
+        exchange_code: '544028',
+        sector: 'Technology'
+      },
+      {
+        name: 'BLS E-Services',
+        purchase_price: 232,
+        quantity: 191,
+        exchange: 'BSE',
+        exchange_code: '544107',
+        sector: 'Technology'
+      },
+      {
+        name: 'Tanla',
+        purchase_price: 1134,
+        quantity: 45,
+        exchange: 'BSE',
+        exchange_code: '532790',
+        sector: 'Technology'
+      },
+      {
+        name: 'Dmart',
+        purchase_price: 3777,
+        quantity: 27,
+        exchange: 'NSE',
+        exchange_code: 'DMART',
+        sector: 'Consumer'
+      },
+      {
+        name: 'Tata Consumer',
+        purchase_price: 845,
+        quantity: 90,
+        exchange: 'BSE',
+        exchange_code: '532540',
+        sector: 'Consumer'
+      },
+      {
+        name: 'Pidilite',
+        purchase_price: 2376,
+        quantity: 36,
+        exchange: 'BSE',
+        exchange_code: '500331',
+        sector: 'Consumer'
+      },
+      {
+        name: 'Tata Power',
+        purchase_price: 224,
+        quantity: 225,
+        exchange: 'BSE',
+        exchange_code: '500400',
+        sector: 'Power'
+      },
+      {
+        name: 'KPI Green',
+        purchase_price: 875,
+        quantity: 50,
+        exchange: 'BSE',
+        exchange_code: '542323',
+        sector: 'Power'
+      },
+      {
+        name: 'Suzlon',
+        purchase_price: 44,
+        quantity: 450,
+        exchange: 'BSE',
+        exchange_code: '532667',
+        sector: 'Power'
+      },
+      {
+        name: 'Gensol',
+        purchase_price: 998,
+        quantity: 45,
+        exchange: 'BSE',
+        exchange_code: '542851',
+        sector: 'Power'
+      },
+      {
+        name: 'Hariom Pipes',
+        purchase_price: 580,
+        quantity: 60,
+        exchange: 'BSE',
+        exchange_code: '543517',
+        sector: 'Pipe'
+      },
+      {
+        name: 'Astral',
+        purchase_price: 1517,
+        quantity: 56,
+        exchange: 'NSE',
+        exchange_code: 'ASTRAL',
+        sector: 'Pipe'
+      },
+      {
+        name: 'Polycab',
+        purchase_price: 2818,
+        quantity: 28,
+        exchange: 'BSE',
+        exchange_code: '542652',
+        sector: 'Pipe'
+      },
+      {
+        name: 'Clean Science',
+        purchase_price: 1610,
+        quantity: 32,
+        exchange: 'BSE',
+        exchange_code: '543318',
+        sector: 'Others'
+      },
+      {
+        name: 'Deepak Nitrite',
+        purchase_price: 2248,
+        quantity: 27,
+        exchange: 'BSE',
+        exchange_code: '506401',
+        sector: 'Others'
+      },
+      {
+        name: 'Fine Organic',
+        purchase_price: 4284,
+        quantity: 16,
+        exchange: 'BSE',
+        exchange_code: '541557',
+        sector: 'Others'
+      },
+      {
+        name: 'Gravita',
+        purchase_price: 2037,
+        quantity: 8,
+        exchange: 'BSE',
+        exchange_code: '533282',
+        sector: 'Others'
+      },
+      {
+        name: 'SBI Life',
+        purchase_price: 1197,
+        quantity: 49,
+        exchange: 'BSE',
+        exchange_code: '540719',
+        sector: 'Others'
+      },
+      {
+        name: 'Infy',
+        purchase_price: 1647,
+        quantity: 36,
+        exchange: 'BSE',
+        exchange_code: '500209',
+        sector: 'Others'
+      },
+      {
+        name: 'Happiest Minds',
+        purchase_price: 1103,
+        quantity: 45,
+        exchange: 'BSE',
+        exchange_code: '543237',
+        sector: 'Others'
+      },
+      {
+        name: 'EaseMyTrip',
+        purchase_price: 20,
+        quantity: 1332,
+        exchange: 'BSE',
+        exchange_code: '543272',
+        sector: 'Others'
+      }
+]
